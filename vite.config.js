0e068a955beb7266b51f2d2a6e7a049b851973ba
abdoulaye05 <abdoulaye.diallo02@edu.univ-fcomte.fr>
2025-04-21 18:07:27 +0200

    nouveau commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // 👈 très important pour GitHub Pages
   plugins: [react()],
 })
