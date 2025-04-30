29acac9a90a04e96c7d33cadbd45b6d624bb0d84
abdoulaye05 <abdoulaye.diallo02@edu.univ-fcomte.fr>
2025-04-30 16:28:39 +0200

    nouveau commit 16:28

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-export default defineConfig({
-  base: isProduction ? '/portfolio/' : '/',
-  plugins: [react()],
-});
